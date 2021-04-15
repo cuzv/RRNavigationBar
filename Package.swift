9c1ccb8948b0031be543c0e341c4eecde4b94baa
Shaw <cuzval@gmail.com>
2021-04-15 09:47:48 +0800

    Support SPM

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RRNavigationBar",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(name: "RRNavigationBar", targets: ["RRNavigationBar"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "RRNavigationBar", dependencies: [], path: "Sources"),
+    ]
+)
