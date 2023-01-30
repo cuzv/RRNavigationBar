6e3ee458d27600e994004cbc747f3531d8e2a5af
Shaw <cuzval@gmail.com>
2023-01-30 20:48:53 +0800

    Integrate Carthage use in Swift project

--- Sources/Public/RRNavigationBar.h
+++ Sources/Public/RRNavigationBar.h
@@ -6,11 +6,13 @@
 //  Copyright © 2017 Shaw. All rights reserved.
 //
 
-#ifndef RRNavigationBar_h
-#define RRNavigationBar_h
-
+#import <Foundation/Foundation.h>
+#if __has_include(<RRNavigation/UIViewController+RRNavigationBar.h>)
+#import <RRNavigation/UIViewController+RRNavigationBar.h>
+#import <RRNavigation/UINavigationBar+RRNavigationBar.h>
+#import <RRNavigation/UINavigationController+RRNavigationBar.h>
+#else
 #import "UIViewController+RRNavigationBar.h"
 #import "UINavigationBar+RRNavigationBar.h"
 #import "UINavigationController+RRNavigationBar.h"
-
-#endif /* RRNavigationBar_h */
+#endif
